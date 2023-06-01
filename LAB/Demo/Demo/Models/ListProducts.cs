fec2610e417c41b982fcc25d52305b526fde4268
ThaoVy <134570541+ThaoVy2k2@users.noreply.github.com>
2023-06-01 15:42:30 +0700

    205051960

---
+++ LAB/Demo/Demo/Models/ListProducts.cs
@@ -0,0 +1,19 @@
+﻿using Demo.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Models
+{
+    public class ListProducts : ViewComponent
+    {
+        private readonly DemoContext _context;
+
+        public ListProducts(DemoContext context)
+        {
+            _context = context;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(_context.Category.ToList());
+        }
+    }
+}
---
+++ LAB/Demo/Demo/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Demo.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [System.ComponentModel.DisplayName("Tên sản phẩm")]
+        public string? Name { get; set; }
+        [System.ComponentModel.DisplayName("Mô tả sản phẩm ")]
+        public string? Description { get; set; }
+        public string? ImageUrl { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+
+        [ForeignKey("Category")]
+        public int CategoryId { get; set; }
+
+        public virtual Category? category { get; set; }
+    }
+}
